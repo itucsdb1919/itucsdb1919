d9b385ca1a711027999a5f68b578dbff0f0fba42
Habib Bartu Gökalp <habibbartugokalp@Habib-MacBook-Pro.local>
2019-11-02 20:47:09 +0300

    congif timeeee

--- dbinit.py
+++ dbinit.py
@@ -11,9 +11,7 @@ INIT_STATEMENTS = [
 
 
 def initialize(url):
-    print ("test debug")
-    print("test debug muhammed")
-    with dbapi2.connect(url , sslmode='require') as connection:
+    with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
         for statement in INIT_STATEMENTS:
             cursor.execute(statement)
@@ -21,8 +19,8 @@ def initialize(url):
 
 
 if __name__ == "__main__":
-    url = "postgres://lwgysxzadqznqz:1d99ac08fda0c54c8e686f0057d88e65b9171c5bc3684551980e9b75ace378b9@ec2-54-217-235-87.eu-west-1.compute.amazonaws.com:5432/dfpj1pes2t0cba"
+    url = os.getenv("DATABASE_URL")
     if url is None:
-        print("Usage: DATABASE_URL=url python dbtest.py", file=sys.stderr)
+        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
     initialize(url)
