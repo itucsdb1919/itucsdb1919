5e439b63725b4175faf2c1b66d477b019038ed3e
Habib Bartu Gökalp <habibbartugokalp@Habib-MacBook-Pro.local>
2019-11-03 01:07:08 +0300

    tables created

--- dbinit.py
+++ dbinit.py
@@ -5,8 +5,39 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """
+    CREATE TABLE "Brand" (
+      "brand_name" text PRIMARY KEY,
+      "web_site" text,
+      "logo_url" text,
+      "year_founded" int,
+      "segment" text,
+      "net_worth" float,
+      "country" text,
+      "city" text
+    );
+
+    CREATE TABLE "Model" (
+      "model_id" SERIAL PRIMARY KEY,
+      "model_name" text,
+      "year" int,
+      "bike_type" text,
+      "frame_material" text,
+      "target_customer" text,
+      "brand" text,
+      "country" text
+    );
+    
+    CREATE TABLE "Parts" (
+      "parts_id" SERIAL PRIMARY KEY,
+      "gidon" text,
+      "aktarici" text,
+      "sele" text,
+      "jant" text,
+      "lastik" text,
+      "pedal" text
+    );
+    """
 ]
 
 
