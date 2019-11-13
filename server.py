f993eb77a2c9847eab38c1fdfaa77e4727b8d3d9
Habib Bartu Gökalp <habibbartugokalp@Habib-MacBook-Pro.local>
2019-11-13 17:18:06 +0300

    database access

--- server.py
+++ server.py
@@ -1,18 +1,38 @@
 from flask import Flask,render_template
 import psycopg2 as dbapi2
-from config import config
-
 
 def connect():
     # Connect to the PostgreSQL database server
-    conn = None
     try:
         # read connection parameters
-        params = config()
 
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
+        conn = dbapi2.connect(host="localhost", database="postgres", user="postgres", password="docker")
+
+        cur = conn.cursor()
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # display tables
+        print('PostgreSQL database tables:')
+        cur.execute("""SELECT table_name FROM information_schema.tables
+               WHERE table_schema = 'public'""")
+        for table in cur.fetchall():
+            print(table)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, dbapi22.DatabaseError) as error:
+        print(error)
+
+if __name__ == '__main__':
+    connect()
 
 app = Flask(__name__)
 
@@ -23,3 +43,6 @@ def home_page():
 
 if __name__ == "__main__":
     app.run()
+
+
+
