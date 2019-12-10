4cafc23ea5d2c5c03432fe5a0c5a2ccce02ec7be
Habib Bartu Gökalp <habibbartugokalp@Habib-MacBook-Pro.local>
2019-12-11 02:38:59 +0300

    table display

--- server.py
+++ server.py
@@ -2,29 +2,35 @@ from flask import Flask,render_template
 import psycopg2 as dbapi2
 
 
-def connect():
+def executeSQL(sqlCode):
     try:
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        connection = dbapi2.connect(host="localhost", database="postgres", user="postgres", password="docker")
+        connection = dbapi2.connect(host="ec2-54-217-235-87.eu-west-1.compute.amazonaws.com", database="dfpj1pes2t0cba",
+                              user="lwgysxzadqznqz", password="1d99ac08fda0c54c8e686f0057d88e65b9171c5bc3684551980e9b75ace378b9")
 
         cursor = connection.cursor()
 
         # Execute SQL code
-        cursor.execute("""SELECT version();""")
-        version = cursor.fetchone()
-        print("Postgress version: ",version)
+        cursor.execute(sqlCode)
+        data = cursor.fetchall()
         cursor.close()
         connection.close()
+        return data
 
     except (Exception, dbapi2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
+
+#def insertBike(title, color, frame_size, price, is_active, parts_id ,owner_nickname, city, country, model_id):
+
+
 
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    connect()
+    for element in executeSQL("""SELECT * FROM \"Country\""""):
+        print(element)
 
 
 @app.route("/")
@@ -33,7 +39,8 @@ def home_page():
 
 @app.route("/bikes" , methods=['GET'])
 def bikes():
-    return render_template("homepage.html")
+    bikes = executeSQL("""SELECT * FROM \"Country\"""")
+    return render_template("deneme.html", bikes = bikes)
 
 
 if __name__ == "__main__":
