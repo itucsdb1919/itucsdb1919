45525b069ddc2999ff09c9146d197cbcdabda688
Muhammed Kozan <kozan16@itu.edu.tr>
2019-12-11 21:19:17 +0300

    server.py insertion added

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request, redirect, url_for
 import psycopg2 as dbapi2
 
 
-def executeSQL(sqlCode):
+def executeSQL(sqlCode,operation):
     try:
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
@@ -11,13 +11,19 @@ def executeSQL(sqlCode):
                               user="lwgysxzadqznqz", password="1d99ac08fda0c54c8e686f0057d88e65b9171c5bc3684551980e9b75ace378b9")
 
         cursor = connection.cursor()
-
+        
         # Execute SQL code
         cursor.execute(sqlCode)
-        data = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return data
+        if(operation == "select"):
+            data = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return data
+        if(operation == "insert"):
+            connection.commit()
+            cursor.close()
+            connection.close()  
+            
 
     except (Exception, dbapi2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
@@ -26,6 +32,7 @@ def executeSQL(sqlCode):
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
 def home_page():
@@ -33,35 +40,47 @@ def home_page():
 
 @app.route("/bikes")
 def bike_page():
-    bikes = executeSQL("Select T1.title, T1.color,T1.owner_nickname, T3.image_url from \"Bikes\" as T1 LEFT JOIN \"Model\" as T2 ON T1.model_id = T2.model_id LEFT JOIN \"Bike_images\" as T3 ON T1.bike_id = T3.bike_id  WHERE T1.is_active ='yes'")
+    bikes = executeSQL("Select T1.title, T1.color,T1.owner_nickname, T3.image_url from \"Bikes\" as T1 LEFT JOIN \"Model\" as T2 ON T1.model_id = T2.model_id LEFT JOIN \"Bike_images\" as T3 ON T1.bike_id = T3.bike_id  WHERE T1.is_active ='yes'","select")
     return render_template("bikes.html", bikes = bikes)
 
 
 @app.route("/cities" , methods=['GET'])
 def statistics_city():
-    statistics = executeSQL("""SELECT * FROM \"City\"""")
+    statistics = executeSQL("""SELECT * FROM \"City\"""","select")
     name = "city"
     return render_template("statistics.html", statistics = statistics, name = name)
 
 @app.route("/countries" , methods=['GET'])
 def statistics_country():
-    statistics = executeSQL("""SELECT * FROM \"Country\"""")
+    statistics = executeSQL("""SELECT * FROM \"Country\"""","select")
     name = "country"
     return render_template("statistics.html", statistics = statistics, name = name)
 
 @app.route("/brands" , methods=['GET'])
 def statistics_brand():
-    statistics = executeSQL("""SELECT * FROM \"Brand\"""")
+    statistics = executeSQL("""SELECT * FROM \"Brand\"""","select")
     name = "brand"
     return render_template("statistics.html", statistics = statistics, name = name)
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET','POST'])
 def signup_page():
-    return render_template("signup.html",operation="register")
+    if request.method == "POST":
+        name = request.form['name']
+        surname = request.form['surname']
+        nickname = request.form['nickname']
+        image_url = request.form['image_url']
+        print(name,surname,nickname,image_url)
+        query = "INSERT INTO \"Profil\" (\"name\", \"surname\", \"profil_nickname\", \"profil_image\", \"number_of_bikes\", \"number_of_deals\")VALUES ('" + name + "', '" + surname + "', '"+ nickname + "','" + image_url + "', '0', '0')"
+        print(query)
+        executeSQL(query,"insert")
+        return redirect(url_for('home_page'))
+        
+    else:
+         return render_template("signup.html")
 
-@app.route("/signin")
+@app.route("/signin", methods=['GET','POST'])
 def signin_page():
-    return render_template("signup.html",operation="login")
+    return render_template("signin.html")
 
 if __name__ == "__main__":
     app.run()
