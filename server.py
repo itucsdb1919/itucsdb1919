9dee90011ae7745ca4a15db75468690921e648f6
Habib Bartu Gökalp <habibbartugokalp@Habib-MacBook-Pro.local>
2019-12-11 14:33:13 +0300

    pickle update

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ def executeSQL(sqlCode):
     try:
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-
         connection = dbapi2.connect(host="ec2-54-217-235-87.eu-west-1.compute.amazonaws.com", database="dfpj1pes2t0cba",
                               user="lwgysxzadqznqz", password="1d99ac08fda0c54c8e686f0057d88e65b9171c5bc3684551980e9b75ace378b9")
 
@@ -25,7 +24,14 @@ def executeSQL(sqlCode):
 #def insertBike(title, color, frame_size, price, is_active, parts_id ,owner_nickname, city, country, model_id):
 
 
+
+
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    for element in executeSQL("""SELECT * FROM \"Country\""""):
+        print(element)
+
 
 @app.route("/")
 def home_page():
